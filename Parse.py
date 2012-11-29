7ddb4c8d22a4803505e4dcfeb13d810c5b7fb744
WillMorrison <camocrazed@gmail.com>
2012-11-29 11:58:56 -0500

    added range parsing and reference parsing

--- Parse.py
+++ Parse.py
@@ -1,16 +1,22 @@
 import itertools
 import sys
+import NumRange
 
 ################################################################################
-# This takes an iterator over tokens and constructs a list, that when passed to
-# itertools.product, will produce an iterator over the output lines. References
-# are not taken, but are stored so that they can be done later.
+# Purpose: Turn a list of tokens into two lists. The first list is designed to 
+#          be passed to itertools.product, and the second list will be used to
+#          perform references on the tuples returned by itertools.product.
+#
+# Arguments: one iterator over tokens
+#
+# Returns: Two lists, a list of iterables and a reference map
 ################################################################################
 def Parse(itoken):
 	toplevel = []
 	listlevel = []
 	rangelevel = []
 	reflevel = []
+	expansionmap = []
 
 	for token in itoken:
 		# the following print statement is DEBUG code
@@ -19,18 +25,20 @@ def Parse(itoken):
 		# if an unexpected character is encountered, 
 		if (token[0] == 'UNEXPECTED'):
 			print >> sys.stderr, "Syntax error: Unexpected '{}' at position {} in context '{}'".format(token[1], token[3], token[2])
-			return None
+			return None, None
 
 		# deal with normal strings appearing at the top level
 		elif (token[0] == 'NORMAL' and token[2] == 'top'):
-			toplevel.append(iter([token[1]]))
+			toplevel.append([token[1]])
+			expansionmap.append(len(toplevel))
 
 		# deal with tokens appearing in list context
 		elif (token[2] == 'list'):
 			listlevel.append(token)
 		#deal with end of list
 		elif (token[0] == 'LIST_CLOSE'):
-			toplevel.append(itertools.imap(lambda x: x[1], itertools.ifilter(lambda x: x[0]=='NORMAL', listlevel)))
+			toplevel.append(map(lambda x: x[1], filter(lambda x: x[0]=='NORMAL', listlevel)))
+			expansionmap.append(len(toplevel))
 			listlevel = []
 
 		#deal with tokens in range context
@@ -38,8 +46,37 @@ def Parse(itoken):
 			rangelevel.append(token)
 		#deal with end of range
 		elif (token[0] == 'RANGE_CLOSE'):
+			# check that we have the correct number of arguments
+			numbers = filter(lambda x: x[0]=='NUMBER', rangelevel)
+			if len(numbers) is 2:
+				seq = map(lambda x: x[1], numbers)
+			elif len(numbers) is 3:
+				seq = map(lambda x: x[1], numbers[0:1]) + int(numbers[2][1])
+			else:
+				print >> sys.stderr, "Syntax error: Incorrect number of elements in range ending at position {}".format(token[3])
+				return None, None
 
+			# construct a range here and append it to toplevel or listlevel
+			if (token[2] == 'top'):
+				toplevel.append(NumRange.NumRange(*seq))
+				expansionmap.append(len(toplevel))
+			elif(token[2] == 'list'):
+				listlevel.append(NumRange.NumRange(*seq))
 			rangelevel = []
 
-	return toplevel
+		#deal with tokens in ref context
+		elif (token[2] == 'reference'):
+			reflevel.append(token)
+		#deal with end of range
+		elif (token[0] == 'REF_CLOSE'):
+			# check that we have the correct number of arguments
+			numbers = filter(lambda x: x[0]=='NUMBER', reflevel)
+			if len(numbers) is not 1:
+				print >> sys.stderr, "Syntax error: Multiple elements in reference ending at position {}".format(token[3])
+				return None, None
+
+			expansionmap.append(int(numbers[0][1]))
+			reflevel = []
+
+	return toplevel, expansionmap
 
--- testParse.py
+++ testParse.py
@@ -3,10 +3,11 @@ import Tokenize
 import Parse
 
 if (len(sys.argv) > 1):
-	lines = Parse.Parse(Tokenize.Tokenize(sys.argv[1]))
+	lines, expansionmap = Parse.Parse(Tokenize.Tokenize(sys.argv[1]))
 	if (lines is not None):
 		for line in lines:
 			print ','.join(line)
+		print "expansionmap is", expansionmap
 else:
 	print >> sys.stderr, "Error: An argument is required"
 
